606768e87868120863ad047a0e846b011b7e4928
MattRenda <51567160+MattRenda@users.noreply.github.com>
2026-06-02 01:13:16 -0700

    Add files via upload

--- server/routes/pusher-listener.js
+++ server/routes/pusher-listener.js
@@ -1,4 +1,5 @@
-import Pusher from 'pusher-js/node.js';
+import PusherClient from 'pusher-js/node.js';
+const Pusher = PusherClient.default || PusherClient;
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
